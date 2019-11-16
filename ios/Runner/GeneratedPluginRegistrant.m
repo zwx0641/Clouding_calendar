135851007a7a0e4d60abf880de1f4725c9a7630a
Zeno WX <zwx4231009@gmail.com>
2019-11-16 19:54:34 +0800

    Revert "Revert "注销接口，加入redis，mybatis""
    This reverts commit 04a2bb9eb119b55d28d09c68705d5f1c03b00230.

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <fluttertoast/FluttertoastPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
 }
 
 @end
