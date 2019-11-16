ab86294caf8eb60072a5bdc66395e68ef05ac179
Zeno WX <zwx4231009@gmail.com>
2019-11-16 20:25:52 +0800

    asas

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,12 +4,14 @@
 
 #import "GeneratedPluginRegistrant.h"
 #import <fluttertoast/FluttertoastPlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
 #import <shared_preferences/SharedPreferencesPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
 }
 
