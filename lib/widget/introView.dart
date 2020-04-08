18f5166f0241bba8fd5e836752e6bfaabd03cdbc
Zeno WX <zwx4231009@gmail.com>
2020-04-08 20:00:52 +0800

    Pictures

--- lib/widget/introView.dart
+++ lib/widget/introView.dart
@@ -6,30 +6,30 @@ final pages = [
     PageViewModel(
         pageColor: const Color(0xFF03A9F4),
         // iconImageAssetPath: 'images/air-hostess.png',
-        bubble: Image.asset('images/air-hostess.png'),
+        bubble: Image.asset('images/calendar_small.png'),
         body: Text(
           'Beautiful calendar views, easy to use',
         ),
         title: Text(
-          'Calendar',
+          'Welcome to Zalendar',
         ),
         titleTextStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
         bodyTextStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
         mainImage: Image.asset(
-          'images/airplane.png',
+          'images/welcomeintro.png',
           height: 285.0,
           width: 285.0,
           alignment: Alignment.center,
         )),
     PageViewModel(
       pageColor: const Color(0xFF8BC34A),
-      iconImageAssetPath: 'images/waiter.png',
+      iconImageAssetPath: 'images/reminder_small.png',
       body: Text(
         'Just a single tap, never forget things',
       ),
       title: Text('Reminders'),
       mainImage: Image.asset(
-        'images/hotel.png',
+        'images/reminderintro.png',
         height: 285.0,
         width: 285.0,
         alignment: Alignment.center,
@@ -39,13 +39,13 @@ final pages = [
     ),
     PageViewModel(
       pageColor: const Color(0xFF607D8B),
-      iconImageAssetPath: 'images/taxi-driver.png',
+      iconImageAssetPath: 'images/event_small.png',
       body: Text(
         'Keep your emergencies in our app, we will remember everything for you',
       ),
       title: Text('Events'),
       mainImage: Image.asset(
-        'images/taxi.png',
+        'images/eventintro.png',
         height: 285.0,
         width: 285.0,
         alignment: Alignment.center,
